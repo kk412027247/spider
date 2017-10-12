4da36c55db38e7405bab4314224eda3c9a5e2e52
tmd <123456>
2017-10-12 23:35:03 +0800

    spider

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const url = require('url');
 
 //引入superAgent
 const superAgent = require('superagent');
@@ -10,16 +11,20 @@ const cheerio = require('cheerio');
 const cNodeUrl = 'http://cNodeJs.org';
 
 router.get('/', (req, res, next) => {
-  superAgent.get(cNodeUrl).end((err, sRes)=>{
+  superAgent.get(cNodeUrl)
+  .end((err, sRes)=>{
     if(err) return next(err) ;
     const $ = cheerio.load(sRes.text);
     const items = [];
-    $('#topic_list .topic_title').each((idx, element)=>{
+    $('#topic_list .topic_title').each((index, element)=>{
       const $element = $(element);
       items.push({
-        title:$element.attr('title')
+        title: $element.attr('title'),
+        href: $element.attr('href'),
+        link: url.resolve(cNodeUrl, $element.attr('href'))
       })
-    })
+    });
+    res.send(items)
   })
 });
 
